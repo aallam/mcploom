5eaac4443d608c7c22a34e44d572757c1ba0dff1
Mouaad Aallam <mouaad@aallam.com>
2026-03-23 22:26:57 +0100

    chore: tighten linting safeguards

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import eslintComments from "@eslint-community/eslint-plugin-eslint-comments";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import jsdoc from "eslint-plugin-jsdoc";
+import security from "eslint-plugin-security";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -18,12 +20,21 @@ export default tseslint.config(
   ...tseslint.configs.recommended,
   {
     files: ["**/*.ts"],
+    plugins: {
+      "@eslint-community/eslint-comments": eslintComments,
+      security,
+    },
     languageOptions: {
       ecmaVersion: "latest",
       globals: globals.node,
       sourceType: "module",
     },
     rules: {
+      "@eslint-community/eslint-comments/disable-enable-pair": "error",
+      "@eslint-community/eslint-comments/no-aggregating-enable": "error",
+      "@eslint-community/eslint-comments/no-duplicate-disable": "error",
+      "@eslint-community/eslint-comments/no-unlimited-disable": "error",
+      "@eslint-community/eslint-comments/no-unused-enable": "error",
       "@typescript-eslint/consistent-type-imports": [
         "error",
         {
@@ -39,6 +50,10 @@ export default tseslint.config(
           varsIgnorePattern: "^_",
         },
       ],
+      "security/detect-bidi-characters": "error",
+      "security/detect-child-process": "error",
+      "security/detect-eval-with-expression": "error",
+      "security/detect-non-literal-require": "error",
     },
   },
   {
